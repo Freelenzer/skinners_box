e9e3d3bf8ab36783b19b970c146423a950d04a51
Freelenzer <freelenzer@posteo.de>
2020-12-21 10:44:20 +0100

    Update app

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "93d5a6eac40857c00d67f708c3d34611",
-"index.html": "0ea364101904c64476b1f7e5582649a9",
-"/": "0ea364101904c64476b1f7e5582649a9",
-"main.dart.js": "09974175cbd2d7606364b26c18093482",
+"index.html": "e2fb9bd88a3215818c1d6898580ee30c",
+"/": "e2fb9bd88a3215818c1d6898580ee30c",
+"main.dart.js": "dd17fda4155395f262c3dd79a94667ed",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
